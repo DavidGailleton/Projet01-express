b9b1a1120e40a89adbb05158b2f75ca9cbac8864
DavidGailleton <david.gailleton@posteo.net>
2023-04-03 13:18:29 +0200

    panier_mis_en_place

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import mysql from 'mysql2/promise';
+
+async function main() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'notSecureChangeMe',
+    database: 'DAGA'
+  });
+
+  const app = express();
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(express.static('public'))
+  app.set('view engine', 'ejs')
+
+  app.get('/', function (httpRequest, httpResponse) {
+    httpResponse.render('pages/index')
+  });
+
+  app.get('/panier', async function (httpRequest, httpResponse) {
+    const [rows, fields] = await connection.execute('SELECT * FROM orders');
+    console.log(`products`, rows);
+    httpResponse.render('pages/panier', { orders: rows });
+  });
+
+  app.post('/clear-orders', async function (req, res, next) {
+
+    await connection.execute('TRUNCATE TABLE orders');
+    res.redirect('/panier');
+
+  });
+
+  app.post('/process-order', async function (req, res, next) {
+      // Clear the orders table
+      await connection.execute('TRUNCATE TABLE orders');
+      // Redirect to the home page
+      res.redirect('/confirm');
+  });
+
+  app.get('/confirm', function (httpRequest, httpResponse) {
+    httpResponse.render('pages/confirm')
+    setTimeout(function () {
+      httpResponse.redirect('/');
+    }, 5000);
+  });
+
+  app.get('/contact', function (httpRequest, httpResponse) {
+    httpResponse.render('pages/contact')
+  });
+  app.get('/simulateur', async function (httpRequest, httpResponse, next) {
+      const [rows, fields] = await connection.execute('SELECT * FROM products');
+      console.log(`products`, rows);
+      httpResponse.render('pages/simulateur', { products: rows });
+  });
+
+  app.post('/simulateur', async function (req, res, next) {
+    const itemName = req.body.item_name;
+    const price = req.body.price;
+    const sql = `INSERT INTO orders (item_name, price) VALUES (?, ?)`;
+    try {
+      const [result] = await connection.execute(sql, [itemName, price]);
+      console.log(result);
+      res.redirect('/panier');
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
 
 
-const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('public'))
-app.set('view engine','ejs')
 
-app.get ('/', function (httpRequest, httpResponse, next )
-{
-  //log de la requete entrante
-  console.log('objet request: ', httpRequest);
-  //envoie de la reponse http
-  httpResponse.render('pages/index')
-});
-app.get ('/contact', function (httpRequest, httpResponse, next ) {
-  httpResponse.render('pages/contact')
-});
-app.get ('/simulateur', function (httpRequest, httpResponse, next ) {
-  httpResponse.render('pages/simulateur')
-});
+  app.listen('3003');
 
-app.listen('3003');
+}
+
+main()
